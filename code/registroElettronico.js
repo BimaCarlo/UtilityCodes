af1b250a1148c988bf84963c79cdc9017c4f3d15
Bima Carlo <71249029+BimaCarlo@users.noreply.github.com>
2021-05-30 15:16:26 +0200

    Update registroElettronico.js

--- code/registroElettronico.js
+++ code/registroElettronico.js
@@ -1,44 +1,42 @@
-
 console.clear();
-let tabellaVoti = document.getElementById('table-voti');
-let body = tabellaVoti.children['tbody'];
-body = tabellaVoti.children[1];
-
-let righe = body.children;
-let voti = []; for(let riga of righe) { voti.push({
-
-materia : riga.children[1].innerHTML,
-voto:riga.children[3].children[0].innerHTML
-
-});}
-
-let sommaVoti = 0;for(let i =0; i<voti.length; i++){ sommaVoti += parseFloat(voti[i].voto)} sommaVoti = sommaVoti/voti.length;console.log("Hai preso " +voti.length + " voti e la tua media è " + sommaVoti);
-
-
-let materie = []; let votiSommati = []; let numeroVoti=[];for(let i =0; i<voti.length; i++){let indice = materie.indexOf(voti[i].materia);if(indice == -1) {materie.push(voti[i].materia); votiSommati.push(parseFloat(voti[i].voto));numeroVoti.push(1);}else{votiSommati[indice] += parseFloat(voti[i].voto);numeroVoti[indice] += 1;}};
-//for(let i = 0; i<materie.length; i++){console.log(materie[i] + ' : ' + (votiSommati[i]/numeroVoti[i]) + ' (' + numeroVoti[i]+')');}
-
-let votiTabellaObj = [];
-for(let i=0; i<materie.length; i++)
+let voti = Array.from(document.querySelectorAll('#table-voti tbody tr')).map((item, index) => {return {
+materia : item.children[1].innerHTML,
+voto:item.children[3].children[0].innerHTML.replaceAll(',','.').replaceAll(' ', '')
+}});
+let sommaVoti = 0;
+voti.forEach((item) => {sommaVoti += parseFloat(item.voto)});
+console.log("Hai preso " +voti.length + " voti e la tua media è " + sommaVoti/voti.length);
+let materie = []; 
+let votiSommati = []; 
+let numeroVoti=[];
+for(let i =0; i<voti.length; i++)
 {
-    votiTabellaObj.push({
+    let indice = materie.indexOf(voti[i].materia);
+    if(indice == -1) 
+    {
+        materie.push(voti[i].materia); 
+        votiSommati.push(parseFloat(voti[i].voto));
+        numeroVoti.push(1);
+    }
+    else
+    {
+        votiSommati[indice] += parseFloat(voti[i].voto);
+        numeroVoti[indice] += 1;
+    }
+}
+let votiTabellaObj = materie.map((item, i) => {
+return {
     materia : materie[i],
     voto:(votiSommati[i]/numeroVoti[i]),
     numeroVoti:numeroVoti[i]
-    });
-}
-
+    } });
 let mediaSommaMaterie = 0;
-materie.map((item, index)=>{ mediaSommaMaterie += parseFloat(votiSommati[index]/numeroVoti[index])});
+materie.forEach((item, index)=>{ mediaSommaMaterie += parseFloat(votiSommati[index]/numeroVoti[index])});
 mediaSommaMaterie = mediaSommaMaterie / materie.length;
-
 console.log("La media tra le medie è : " + mediaSommaMaterie.toString());
-
-let valFinali = {
+votiTabellaObj.push({
     materia:"Totale",
     voto: sommaVoti,
     numeroVoti:numeroVoti.reduce((a,b)=>a+b, 0)
-};
-
-votiTabellaObj.push(valFinali );
+})
 console.table(votiTabellaObj);
